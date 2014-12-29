1402fffcaae1c7b13649118984f603338c031e24
Dima Sukhikh <coddeys@gmail.com>
2014-12-29 16:30:59 +0300

    Create unwb cookbook

---
+++ cookbooks/unwb/metadata.rb
@@ -0,0 +1,7 @@
+name             'unwb'
+maintainer       'uniweb'
+maintainer_email 'coddeys@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures uniweb-portal'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
