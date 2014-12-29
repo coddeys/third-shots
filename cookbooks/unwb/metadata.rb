2bec176b81bbb789674ea9311e65c865f2baf04e
Dima Sukhikh <coddeys@gmail.com>
2014-12-29 16:38:00 +0300

    Create default recipe

--- cookbooks/unwb/metadata.rb
+++ cookbooks/unwb/metadata.rb
@@ -1,7 +1,8 @@
 name             'unwb'
-maintainer       'uniweb'
+maintainer       'Uniweb'
 maintainer_email 'coddeys@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures uniweb-portal'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+
--- cookbooks/unwb/recipes/default.rb
+++ cookbooks/unwb/recipes/default.rb
@@ -2,7 +2,35 @@
 # Cookbook Name:: unwb
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, Uniweb
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# update package database
+include_recipe "apt"
+
+# install packages
+package "telnet"
+package "postfix"
+package "curl"
+package "git-core"
+package "zlib1g-dev"
+package "libssl-dev"
+package "libreadline-dev"
+package "libyaml-dev"
+package "libsqlite3-dev"
+package "sqlite3"
+package "libxml2-dev"
+package "libxslt1-dev"
+package "libpq-dev"
+package "build-essential"
+package "tree"
+
+bash "set timezone" do
+  code <<-EOH
+    echo 'Europe/Moscow' > /etc/timezone
+    dpkg-reconfigure -f noninteractive tzdata
+  EOH
+  not_if "date | grep -q 'MSK'"
+end
